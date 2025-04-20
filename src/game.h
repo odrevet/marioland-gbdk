27521d289bb529e130b160754efce0720e06293a
odrevet <odrevet@gmail.com>
2025-04-20 11:49:19 +0200

    Revert "move some variables from player tp game file"
    This reverts commit 197c253ffd03f4600084c51dc1eacc1ca3a162da.

--- src/game.h
+++ src/game.h
@@ -21,48 +21,14 @@
 #include "sound.h"
 #include "text.h"
 #include "player.h"
-#include "level.h"
 
-extern uint8_t coins;
-extern uint16_t score;
-extern uint8_t joy;
-extern uint16_t time;
-extern uint8_t lives;
-extern uint8_t level_index;
-extern uint8_t joypad_previous, joypad_current;
-extern uint8_t nb_col;
+void init();
+void pause(); 
+void die();
 
 void hud_update_coins();
 void hud_update_score();
 void hud_update_time();
 void hud_update_lives();
-
-inline bool is_coin(uint8_t tile) { return tile == TILE_COIN; }
-
-inline void on_get_coin(uint8_t x, uint8_t y) {
-  uint16_t index = ((y / TILE_SIZE - DEVICE_SPRITE_OFFSET_Y) * MAP_BUFFER_WIDTH) + 
-                   (((x + camera_x) / TILE_SIZE) % MAP_BUFFER_WIDTH);
-  //map_buffer[index] = TILE_EMPTY;
-
-  set_bkg_tile_xy(((x + camera_x) / TILE_SIZE) % DEVICE_SCREEN_BUFFER_WIDTH,
-                  y / TILE_SIZE - DEVICE_SPRITE_OFFSET_Y, TILE_EMPTY);
-
-  sound_play_coin();
-
-  coins++;
-  score += 100;
-
-  if (coins == 100) {
-    lives++;
-    coins = 0;
-  }
-
-  hud_update_coins();
-  hud_update_score();
-}
-
-void init();
-void pause(); 
-void die();
 
 #endif
--- src/player.h
+++ src/player.h
@@ -7,13 +7,21 @@
 #include <string.h>
 
 #include "global.h"
-#include "game.h"
 #include "level.h"
 #include "text.h"
 
 #include "graphics/enemies.h"
 #include "graphics/text.h"
 #include "graphics/mario.h"
+
+extern uint8_t coins;
+extern uint16_t score;
+extern uint8_t joy;
+extern uint16_t time;
+extern uint8_t lives;
+extern uint8_t level_index;
+extern uint8_t joypad_previous, joypad_current;
+extern uint8_t nb_col;
 
 // player coords and movements
 extern uint16_t player_x_subpixel;
@@ -45,6 +53,30 @@ extern uint8_t tile_next_2;
 extern uint8_t scroll;
 
 void update_frame_counter();
+
+inline bool is_coin(uint8_t tile) { return tile == TILE_COIN; }
+
+inline void on_get_coin(uint8_t x, uint8_t y) {
+  uint16_t index = ((y / TILE_SIZE - DEVICE_SPRITE_OFFSET_Y) * MAP_BUFFER_WIDTH) + 
+                   (((x + camera_x) / TILE_SIZE) % MAP_BUFFER_WIDTH);
+  //map_buffer[index] = TILE_EMPTY;
+
+  set_bkg_tile_xy(((x + camera_x) / TILE_SIZE) % DEVICE_SCREEN_BUFFER_WIDTH,
+                  y / TILE_SIZE - DEVICE_SPRITE_OFFSET_Y, TILE_EMPTY);
+
+  sound_play_coin();
+
+  coins++;
+  score += 100;
+
+  if (coins == 100) {
+    lives++;
+    coins = 0;
+  }
+
+  hud_update_coins();
+  hud_update_score();
+}
 
 void player_move() BANKED;
 
