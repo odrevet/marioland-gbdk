ebfbaa0c87730cf4c1258aede375a64ea2b326e0
odrevet <odrevet@gmail.com>
2026-01-31 11:15:43 +0100

    fix underground_2

--- src/levels/underground_2.h
+++ src/levels/underground_2.h
@@ -7,8 +7,8 @@ BANKREF_EXTERN(underground_2)
 
 
 
-#define underground_2_sz_comp 10
-#define underground_2_sz_decomp 16
+#define underground_2_sz_comp 94
+#define underground_2_sz_decomp 320
 
 extern const unsigned char underground_2[];
 
