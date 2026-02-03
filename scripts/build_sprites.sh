9bf150185b1214fda25d53af12e67aa75e4dabe2
odrevet <odrevet@gmail.com>
2026-02-03 17:28:39 +0100

    use indexed palette

--- scripts/build_sprites.sh
+++ scripts/build_sprites.sh
@@ -5,15 +5,15 @@ get_define_value() {
 tile_origin=0
 
 echo "mario sprites"
-png2asset "assets/sprites/mario.png" -o "src/graphics/mario.c" -sw 16 -sh 16 -spr8x8 -tile_origin $tile_origin -b 255
+png2asset "assets/sprites/mario.png" -o "src/graphics/mario.c" -sw 16 -sh 16 -spr8x8 -keep_palette_order -tile_origin $tile_origin -b 255
 tile_origin=$((tile_origin + $(get_define_value mario_TILE_COUNT "src/graphics/mario.h")))
 
 echo "enemies sprites"
-png2asset "assets/sprites/enemies.png" -o "src/graphics/enemies.c" -sw 8 -sh 16 -spr8x8 -tile_origin $tile_origin -b 255
+png2asset "assets/sprites/enemies.png" -o "src/graphics/enemies.c" -sw 8 -sh 16 -spr8x8 -keep_palette_order -tile_origin $tile_origin -b 255
 tile_origin=$((tile_origin + $(get_define_value enemies_TILE_COUNT "src/graphics/enemies.h")))
 
 echo "common sprites"
-png2asset "assets/sprites/common.png" -o "src/graphics/sprite_common.c" -sw 8 -sh 8 -spr8x8 -tile_origin $tile_origin -b 255
+png2asset "assets/sprites/common.png" -o "src/graphics/sprite_common.c" -sw 8 -sh 8 -spr8x8 -keep_palette_order -tile_origin $tile_origin -b 255
 tile_origin=$((tile_origin + $(get_define_value sprite_common_TILE_COUNT "src/graphics/sprite_common.h")))
 
 echo "enemies sprites large"
