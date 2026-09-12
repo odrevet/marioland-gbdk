414ffb041341e6f3b75e16eeb11991cf59338543
odrevet <odrevet@gmail.com>
2026-09-12 12:39:19 +0200

    rmove legacy code

--- src/main.c
+++ src/main.c
@@ -204,17 +204,6 @@ void main(void) {
 
   init();
 
-#ifdef NINTENDO_NES
-  uint8_t y;
-  uint8_t initial_tiles[32];
-
-  memset(initial_tiles, TILE_EMPTY, 32);
-
-  for (y = 0; y != 30; y++) {
-    set_bkg_tiles(0, y, 32, 1, initial_tiles);
-  }
-#endif
-
   SHOW_BKG;
 #ifdef GAMEBOY
   SHOW_WIN;
